45b3c118965b0b83976c5e962273d8263ea3315b
Matt Milzewski <mattmilzewski@gmail.com>
2025-04-14 10:49:46 +1000

    update the book

--- wiki.js
+++ wiki.js
@@ -1,21 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // Hide all tab contents
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
+  // Remove the "active" class from all tab buttons
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab and add "active" class to the button that opened it
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Automatically click the first tab on page load
+document.addEventListener("DOMContentLoaded", function () {
   var defaultTab = document.getElementsByClassName("tablinks")[0];
   if (defaultTab) {
     defaultTab.click();
